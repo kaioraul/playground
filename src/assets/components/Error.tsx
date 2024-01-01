4f98b17100f685b0b5bfbaf7bc4a19d350d4c5ed
Miłosz Zając <milosz.zajac@dpdgroup.com>
2024-01-01 21:23:34 +0100

    work in progress

---
+++ src/assets/components/Error.tsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+
+export const Error = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+
+  return (
+    <>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Customer
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+};
--- src/pages/StarWarsPage.tsx
+++ src/pages/StarWarsPage.tsx
@@ -1,27 +1,36 @@
 import { Text, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { PlanetCard } from "./StarWarsPage/PlanetCard";
-
+import { Error } from "../assets/components/Error";
 const url = "http://swapi.dev/api/";
 
 export const StarWarsPage = () => {
   const [presentedPlanets, setPresentedPlanets] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlanets() {
-      const response = await fetch(url + "planets/");
-      const resData = await response.json();
-      setPresentedPlanets(resData);
-    }
-    try {
       setIsLoading(true);
-      fetchPlanets();
-    } catch (error) {
-      return <>error.message</>;
+      try {
+        const response = await fetch(url + "planets/");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch planets.");
+        }
+        setPresentedPlanets(resData.planets);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
     }
-    setIsLoading(false);
+
+    fetchPlanets();
   }, []);
+
+  if (error) {
+    return;
+  }
 
   return (
     <>
@@ -32,7 +41,7 @@ export const StarWarsPage = () => {
       </Text>
       <Text>Your planets</Text>
       {isLoading ? <Spinner /> : <PlanetCard name={"Tatooine"} />}
-
+      {presentedPlanets}
       <Text>
         Using Star Wars Api at http://swapi.dev/api/. Created by Miłosz Zając.
       </Text>
